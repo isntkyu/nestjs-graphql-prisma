e67441b19fcc74a5e8d6c913a01d2c706710759a
이준규 <jkjkjk5710@gmail.com>
2024-06-30 19:54:46 +0900

    baseCrudService

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { GqlJwtGuard } from './guards/gql-jwt.guard';
 import { JwtModule } from '@nestjs/jwt';
-import { PostResolver } from '../modules/post/post.resolver';
-import { UserResolver } from '../modules/user/user.resolver';
 import { PrismaService } from 'src/prisma.service';
 import { UserService } from './services/user.service';
 
@@ -14,6 +12,6 @@ import { UserService } from './services/user.service';
       signOptions: { expiresIn: '2days' },
     }),
   ],
-  providers: [GqlJwtGuard, UserService],
+  providers: [GqlJwtGuard, UserService, PrismaService],
 })
 export class CommonModule {}
