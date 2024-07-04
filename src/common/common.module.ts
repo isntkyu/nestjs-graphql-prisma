6a535857b55a00e0a31c36096fcda8f63293d7a8
이준규 <jkjkjk5710@gmail.com>
2024-07-04 11:32:57 +0900

    refactor

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -2,16 +2,8 @@ import { Module } from '@nestjs/common';
 import { GqlJwtGuard } from './guards/gql-jwt.guard';
 import { JwtModule } from '@nestjs/jwt';
 import { PrismaService } from 'src/prisma.service';
-import { UserService } from './services/user.service';
 
 @Module({
-  imports: [
-    JwtModule.register({
-      global: true,
-      secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '2days' },
-    }),
-  ],
-  providers: [GqlJwtGuard, UserService, PrismaService],
+  providers: [GqlJwtGuard, PrismaService],
 })
 export class CommonModule {}
